d9ffecd9080458c0455d9445cc285d8f83bd674b
rooney <bch3454@gmail.com>
2025-12-05 15:54:10 +0900

    feat: 레이아웃 구조 설계

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,132 @@
+"use client";
+
+import {
+  CreditCardIcon,
+  FolderOpenIcon,
+  HistoryIcon,
+  KeyIcon,
+  LogOutIcon,
+  StarIcon,
+} from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import { authClient } from "@/lib/auth-client";
+
+const menuItems = [
+  {
+    title: "Main",
+    items: [
+      { title: "Workflows", icon: FolderOpenIcon, url: "/workflows" },
+      { title: "Credentials", icon: KeyIcon, url: "/credentials" },
+      { title: "Executions", icon: HistoryIcon, url: "/executions" },
+    ],
+  },
+];
+
+export const AppSidebar = () => {
+  const pathname = usePathname();
+  const router = useRouter();
+
+  return (
+    <Sidebar>
+      <SidebarHeader>
+        <SidebarMenuItem>
+          <SidebarMenuButton asChild className="gap-x-4 h-10 px-4">
+            <Link href="/" prefetch>
+              <Image
+                src="/logos/logo.svg"
+                alt="Nodebase"
+                width={30}
+                height={30}
+              />
+              <span className="font-semibold text-sm">Nodebase</span>
+            </Link>
+          </SidebarMenuButton>
+        </SidebarMenuItem>
+      </SidebarHeader>
+      <SidebarContent>
+        {menuItems.map((group) => (
+          <SidebarGroup key={group.title}>
+            <SidebarGroupContent>
+              <SidebarMenu>
+                {group.items.map((item) => (
+                  <SidebarMenuItem key={item.title}>
+                    <SidebarMenuButton
+                      asChild
+                      isActive={
+                        item.url === "/"
+                          ? pathname === "/"
+                          : pathname.startsWith(item.url)
+                      }
+                      tooltip={item.title}
+                      className="gap-x-4 h-10 px-4"
+                    >
+                      <Link href={item.url} prefetch>
+                        <item.icon className="size-4" />
+                        <span>{item.title}</span>
+                      </Link>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        ))}
+      </SidebarContent>
+      <SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton
+              className="gap-x-4 h-10 px-4"
+              onClick={() => {}}
+              tooltip="Upgrade to Pro"
+            >
+              <StarIcon className="size-4" />
+              <span>Upgrade to Pro</span>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+          <SidebarMenuItem>
+            <SidebarMenuButton
+              className="gap-x-4 h-10 px-4"
+              onClick={() => {}}
+              tooltip="Billing portal"
+            >
+              <CreditCardIcon className="size-4" />
+              <span>Billing portal</span>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+          <SidebarMenuItem>
+            <SidebarMenuButton
+              className="gap-x-4 h-10 px-4"
+              onClick={() =>
+                authClient.signOut({
+                  fetchOptions: {
+                    onSuccess: () => {
+                      router.push("/login");
+                    },
+                  },
+                })
+              }
+              tooltip="Sign out"
+            >
+              <LogOutIcon className="size-4" />
+              <span>Sign out</span>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
+    </Sidebar>
+  );
+};
