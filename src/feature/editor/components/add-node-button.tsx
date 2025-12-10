f136a5e887129607a8ffa2f0d1e75049e39e8dc9
rooney <bch3454@gmail.com>
2025-12-10 17:36:02 +0900

    bugfix: Empty onClick Handlers

--- src/feature/editor/components/add-node-button.tsx
+++ src/feature/editor/components/add-node-button.tsx
@@ -9,12 +9,7 @@ export const AddNodeButton = memo(() => {
   const [selectorOpen, setSelectorOpen] = useState(false);
   return (
     <NodeSelector open={selectorOpen} onOpenChange={setSelectorOpen}>
-      <Button
-        size="icon"
-        variant="outline"
-        onClick={() => {}}
-        className="bg-background"
-      >
+      <Button size="icon" variant="outline" className="bg-background">
         <PlusIcon className="size-4" />
       </Button>
     </NodeSelector>
