a0803bb4ded0135274fd61464cf97193289f9095
rooney <bch3454@gmail.com>
2025-12-22 10:20:19 +0900

    feat: Add credential router

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,10 +1,12 @@
 // import { z } from "zod";
 
+import { credentialsRouter } from "@/feature/credentials/server/routers";
 import { workflowsRouter } from "@/feature/workflows/server/routers";
 import { createTRPCRouter } from "../init";
 
 export const appRouter = createTRPCRouter({
   workflows: workflowsRouter,
+  credentials: credentialsRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
