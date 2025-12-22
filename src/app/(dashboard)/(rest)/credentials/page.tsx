60b3a7b1ca5af59c41db08ecc55bd526fbbc1cdd
rooney <bch3454@gmail.com>
2025-12-22 10:40:27 +0900

    feat: Add Server loader

--- src/app/(dashboard)/(rest)/credentials/page.tsx
+++ src/app/(dashboard)/(rest)/credentials/page.tsx
@@ -1,8 +1,29 @@
+import type { SearchParams } from "nuqs/server";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+import { credentialsParamsLoader } from "@/feature/credentials/server/params-loader";
+import { prefetchCredentials } from "@/feature/credentials/server/prefetch";
 import { requireAuth } from "@/lib/auth-utils";
+import { HydrateClient } from "@/trpc/server";
 
-const Page = async () => {
+type Props = {
+  searchParams: Promise<SearchParams>;
+};
+
+const Page = async ({ searchParams }: Props) => {
   await requireAuth();
-  return <div>Credentials</div>;
+  const params = await credentialsParamsLoader(searchParams);
+  prefetchCredentials(params);
+
+  return (
+    <HydrateClient>
+      <ErrorBoundary fallback={<div>Error</div>}>
+        <Suspense fallback={<div>Loading...</div>}>
+          <div>Credentials</div>
+        </Suspense>
+      </ErrorBoundary>
+    </HydrateClient>
+  );
 };
 
 export default Page;
